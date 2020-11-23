bd3039eeefe687077ec27a1c9291467fac3ef3b5
Adrian Banda <5205bda@gmail.com>
2020-11-22 20:07:01 -0600

    ADD EMPLOYEES/index.txt/schema.sql/seed.sql

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-require("console.table");
+const cTable = require("console.table");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -24,17 +24,27 @@ var connection = mysql.createConnection({
         name: "choices",
         message: "What would you like to do?",
         choices: [
+          "Add Employee",
+          "Add Role",
+          "Add Department",
           "View all employees",
           "View all employees by Department",
           "View all employees by Manager",
           "View all roles ",
           "View all departments",
-          "Add Employee",
-          "Add Role",
           "Exit"]
       })
       .then(function ({ choices }) {
         switch (choices) {
+          case "Add Employee":
+            addEmployee();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+          case "Add Department":
+            addDepartment();
+            break;
           case "View all employees":
              viewAllEmployees();
             break;
@@ -50,12 +60,6 @@ var connection = mysql.createConnection({
           case "View all departments":
             viewAllDepartments();
             break;
-            case "Add Employee":
-                addEmployee();
-                break;
-          case "Add Role":
-            addRole();
-            break;
           case "Exit":
             connection.end();
             break;
@@ -63,30 +67,92 @@ var connection = mysql.createConnection({
       });
   }
 
+  function viewAllEmployees() {
+  
+    const query =
+    `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.names AS department, roles.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+    ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+    ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+  
+      starterQuestions();
+    });
+  }
+
+  //Adding Employee
+  function addEmployee() {
+
+  const query =
+    `SELECT r.id, r.title, r.salary 
+      FROM roles r`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const roleChoices = res.map(({ id, title, salary }) => ({
+      value: id, title: `${title}`, salary: `${salary}`
+    }));
+
+    // console.table(res);
+
+    employeeInsert(roleChoices);
+     });
+  }
+
+  function employeeInsert(roleChoices) {
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employee's first name?"
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the employee's last name?"
+        },
+        {
+          type: "input",
+          name: "manager_id",
+          message: "What is the managers ID?"
+        },
+        {
+          type: "list",
+          name: "roleId",
+          message: "What is the employee's role?",
+          choices: roleChoices
+        },
+      ])
+      .then(function (answer) {
+  
+        const query = `INSERT INTO employee SET ?`
+
+        connection.query(query,
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: answer.roleId,
+            manager_id: answer.manager_id,
+          },
+          function (err, res) {
+            if (err) throw err;
+  
+            // console.table(res);
+  
+            starterQuestions();
+          });
+      });
+  }
 
 
-// Questions
-
-// What would you like to do?
-// View all employees
-// View all employees by department
-// View all employees by Manager **
-
-// View all roles 
-
-// View all departments
-
-// Add Employee
-
-// What is the employes first name? (input)
-// What is the employes last name? (input)
-// What is the employess role? (choices)
-// Sales Lead 
-// Sales Person
-// Lead Engineer 
-// Software Engineer
-// Account Manger 
-// Accountant 
-// Legal Team Lead
- 
-// Whos is the Employees Manager? (Choices)
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,1 +1,28 @@
 USE employeeDB;
+
+-- DEPARTMENT
+INSERT INTO department (names)
+VALUES ("Sales");
+INSERT INTO department (names)
+VALUES ("Engineering");
+INSERT INTO department (names)
+VALUES ("Finance");
+INSERT INTO department (names)
+VALUES ("Legal");
+
+-- ROLES
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 100, 1);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Lead Engineer", 250, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Software Engineer", 200, 2);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Accountant", 300, 3);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Legal Team Lead", 400, 4);
+
+-- 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Adrian", "Banda", 1, 3);
+
